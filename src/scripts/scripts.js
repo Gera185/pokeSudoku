680a58ef925ca326105c808705bef3f707e18b2a
gerard <gerardacostabazaga@gmail.com>
2024-07-09 06:53:11 +0200

    Ya se pone aleatoriamente diferentes preguntas en el borde, también esta hecha la lógica para que solamente aparezcan los pokemon solamente cuando cumplan las condiciones, ahora estoy viendo para que no me de algúnos errores de promesas en consola

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,5 +1,14 @@
 body.addEventListener("click", cerrarMenu);
 
+filaPrompt.forEach(element => {
+    element.innerText = preguntasFila[(Math.floor(Math.random() * (preguntasFila.length)))].texto
+});
+
+columnaPrompt.forEach(element => {
+    element.innerText = preguntasColumna[(Math.floor(Math.random() * (preguntasColumna.length)))].texto
+});
+
+
 btnCasilla.forEach( boton => boton.addEventListener("click", (event) => {
 
     const botonId = event.currentTarget.id; // Conseguimos la id
@@ -10,25 +19,20 @@ btnCasilla.forEach( boton => boton.addEventListener("click", (event) => {
     botonSubmit.addEventListener("click", (event) => {
         event.preventDefault();
         botonSubmit.setAttribute("disabled", "");
-        let pokemonName = text.value;
+        let pokemonName = text.value.toLowerCase();
         
-        try{
-            fetch(URL + pokemonName)
-                .then((response) => response.json())
-                .then(data => añadirPokemon(data, nodoPadre))
-                .catch((error) => {
-                    console.log(error);
-                    abrirCerrarMenu();
-                    nodoPadre.classList.add("error");
-                    setTimeout(function(){
-                        nodoPadre.classList.remove("error")
-                    },1000);
-                })
+        fetch(URL + pokemonName)
+            .then((response) => response.json())
+            .then(data => comprobarPokemon(data, nodoPadre))
+            .catch((error) => {
+                console.log(error);
+                abrirCerrarMenu();
+                nodoPadre.classList.add("error");
+                setTimeout(function(){
+                    nodoPadre.classList.remove("error")
+                },1000);
+            })
             
-        }catch(error) {
-            console.log(error);
-            nodoPadre.classList.add("error");
-        }
         botonSubmit.removeAttribute("disabled", "");
     });
     
@@ -47,6 +51,114 @@ function añadirPokemon(data, nodoPadre) {
     contenedorBingo.append(div);
 }
 
+async function comprobarGeneracion(prompt, url){
+    const res = await fetch(url);
+    const moreData = await res.json();
+    gen = moreData.generation.name
+
+    index = encontrarIndice(prompt.innerText, preguntasFila)
+
+
+    if(gen === preguntasFila[index].id){
+        return true
+    }
+}
+
+function comprobarTipo(prompt, data){
+    index = encontrarIndice(prompt.innerText, preguntasColumna);
+    // console.log(index)
+
+    if(data === preguntasColumna[index].id){
+        return true
+    }
+}
+
+function comprobarPokemon(data, nodoPadre){
+    if(nodoPadre.id === "casilla1"){
+        promptX = document.getElementById("prompt1");
+        promptY = document.getElementById("prompt4");
+
+        console.log(data.types[1])
+
+        if(data.types[1] === undefined){
+            if(comprobarGeneracion(promptX, promptY, data, data.species.url) && comprobarTipo(promptY, data.types[0].type.name)){
+                añadirPokemon(data, nodoPadre)
+            }
+        }else{
+            console.log("Hola")
+            if(comprobarGeneracion(promptX, promptY, data, data.species.url) && (comprobarTipo(promptY, data.types[0].type.name) || comprobarTipo(promptY, data.types[1].type.name))){
+                añadirPokemon(data, nodoPadre)
+            }
+        }
+        
+    }else if(nodoPadre.id === "casilla2"){
+        promptX = document.getElementById("prompt2");
+        promptY = document.getElementById("prompt4");
+
+        if(comprobarGeneracion(promptX, promptY, data, data.species.url) && (comprobarTipo(promptY, data.types[0].type.name) || comprobarTipo(promptY, data.types[1].type.name))){
+            añadirPokemon(data, nodoPadre)
+        }
+    }else if(nodoPadre.id === "casilla3"){
+        promptX = document.getElementById("prompt3");
+        promptY = document.getElementById("prompt4");
+
+        if(comprobarGeneracion(promptX, promptY, data, data.species.url) && (comprobarTipo(promptY, data.types[0].type.name) || comprobarTipo(promptY, data.types[1].type.name))){
+            añadirPokemon(data, nodoPadre)
+        }
+
+    }else if(nodoPadre.id === "casilla4"){
+        promptX = document.getElementById("prompt1");
+        promptY = document.getElementById("prompt5");
+
+        if(comprobarGeneracion(promptX, promptY, data, data.species.url) && (comprobarTipo(promptY, data.types[0].type.name) || comprobarTipo(promptY, data.types[1].type.name))){
+            añadirPokemon(data, nodoPadre)
+        }
+
+    }else if(nodoPadre.id === "casilla5"){
+        promptX = document.getElementById("prompt2");
+        promptY = document.getElementById("prompt5");
+
+        if(comprobarGeneracion(promptX, promptY, data, data.species.url) && (comprobarTipo(promptY, data.types[0].type.name) || comprobarTipo(promptY, data.types[1].type.name))){
+            añadirPokemon(data, nodoPadre)
+        }
+
+    }else if(nodoPadre.id === "casilla6"){
+        promptX = document.getElementById("prompt3");
+        promptY = document.getElementById("prompt5");
+
+        if(comprobarGeneracion(promptX, promptY, data, data.species.url) && (comprobarTipo(promptY, data.types[0].type.name) || comprobarTipo(promptY, data.types[1].type.name))){
+            añadirPokemon(data, nodoPadre)
+        }   
+
+    }else if(nodoPadre.id === "casilla7"){
+        promptX = document.getElementById("prompt1");
+        promptY = document.getElementById("prompt6");
+
+        if(comprobarGeneracion(promptX, promptY, data, data.species.url) && (comprobarTipo(promptY, data.types[0].type.name) || comprobarTipo(promptY, data.types[1].type.name))){
+            añadirPokemon(data, nodoPadre)
+        }
+        
+    }else if(nodoPadre.id === "casilla8"){
+        promptX = document.getElementById("prompt2");
+        promptY = document.getElementById("prompt6");
+
+        if(comprobarGeneracion(promptX, promptY, data, data.species.url) && (comprobarTipo(promptY, data.types[0].type.name) || comprobarTipo(promptY, data.types[1].type.name))){
+            añadirPokemon(data, nodoPadre)
+        }
+    }else if(nodoPadre.id === "casilla9"){
+        promptX = document.getElementById("prompt3");
+        promptY = document.getElementById("prompt6");
+
+        if(comprobarGeneracion(promptX, promptY, data, data.species.url) && (comprobarTipo(promptY, data.types[0].type.name) || comprobarTipo(promptY, data.types[1].type.name))){
+            añadirPokemon(data, nodoPadre)
+        }
+    }
+}
+
+function fetchInfo(URL){
+    return fetch(URL).then((response) => response.json());
+}
+
 function cerrarMenu(evento){
     if (evento.target.closest(".buscador")) return;
     buscador.classList.remove("visible");
@@ -57,5 +169,14 @@ function abrirCerrarMenu(){
         buscador.classList.remove("visible");
     }else{
         buscador.classList.add("visible");
+    }
+}
+
+function encontrarIndice(gen, preguntas){
+    for (let index = 0; index < preguntas.length; index++) {
+        if(preguntas[index].texto===gen){
+            return index
+        }
+        
     }
 }
--- src/scripts/variables-globales.js
+++ src/scripts/variables-globales.js
@@ -2,6 +2,9 @@ botonSubmit = document.getElementById("btn-submit");
 text = document.querySelector("#text-pokemon");
 body = document.querySelector("body");
 
+filaPrompt = document.querySelectorAll(".fila-prompt");
+columnaPrompt = document.querySelectorAll(".columna-prompt");
+
 btnCasilla = document.querySelectorAll(".bingo-boton");
 
 buscador = document.querySelector(".buscador");
@@ -9,3 +12,36 @@ contenedorBingo = document.querySelector(".contenedor-bingo");
 
 
 let URL = "https://pokeapi.co/api/v2/pokemon/";
+
+preguntasFila = [
+                {id:"generation-i", texto:"Primera Generación"},
+                {id:"generation-ii", texto:"Segunda Generación"},
+                {id:"generation-iii", texto:"Tercera Generación"},
+                {id:"generation-iv", texto:"Cuarta Generación"},
+                {id:"generation-v", texto:"Quinta Generación"},
+                {id:"generation-vi", texto:"Sexta Generación"},
+                {id:"generation-vii", texto:"Séptima Generación"},
+                {id:"generation-viii", texto:"Octava Generación"},
+                {id:"generation-ix", texto:"Novena Generación"}
+                ];
+
+preguntasColumna = [
+                    {id:"electric", texto:"Tipo Eléctrico"},
+                    {id:"fire", texto:"Tipo Fuego"},
+                    {id:"steel", texto:"Tipo Acero"},
+                    {id:"water", texto:"Tipo Agua"},
+                    {id:"bug", texto:"Tipo Bicho"},
+                    {id:"dragon", texto:"Tipo Dragón"},
+                    {id:"ghost", texto:"Tipo Fantasma"},
+                    {id:"fairy", texto:"Tipo Hada"},
+                    {id:"ice", texto:"Tipo Hielo"},
+                    {id:"fighting", texto:"Tipo Lucha"},
+                    {id:"normal", texto:"Tipo Normal"},
+                    {id:"grass", texto:"Tipo Planta"},
+                    {id:"psychic", texto:"Tipo Psíquico"},
+                    {id:"rock", texto:"Tipo Roca"},
+                    {id:"dark", texto:"Tipo Siniestro"},
+                    {id:"ground", texto:"Tipo Tierra"},
+                    {id:"poison", texto:"Tipo Veneno"},
+                    {id:"flying", texto:"Tipo Volador"}
+                    ];
